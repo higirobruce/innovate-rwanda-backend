4268684da9d9cd3eace3d4a1dc614fc59da01c77
Aline Abimana <aline@techclick.rw>
2020-12-20 17:24:33 +0200

    correction to counters

--- src/controllers/GenericController.js
+++ src/controllers/GenericController.js
@@ -3,27 +3,23 @@ import db from "../models";
 export default class genericController {
   static async getCounts(req, res) {
     try {
-      const companies = await db['Company'].findAll({
-        attributes: ['status', [db.sequelize.fn('count', db.sequelize.col('status')), 'count']],
-        group: ['Company.status'],
-        raw: true,
-        order: db.sequelize.literal('status DESC')
+      const pendingRequestsCount = await db['Company'].count({
+        where: {
+          status: "pending"
+        }
       });
 
-      const totalCompanies = await db['Company'].count();
+      const usersCount = await db['User'].count();
 
-      const users = await db['User'].findAll({
-        attributes: ['status', [db.sequelize.fn('count', db.sequelize.col('status')), 'count']],
-        group: ['User.status'],
-        raw: true,
-        order: db.sequelize.literal('status DESC')
+      const approvedCompaniesCount = await db['Company'].count({
+        where: {
+          status: "approved"
+        }
       });
-
-      const totalUsers = await db['User'].count();
 
       return res.status(200).json({
         result: {
-          companies, totalCompanies, users, totalUsers
+          pendingRequestsCount, usersCount, approvedCompaniesCount
         }
       })
     } catch (err) {
@@ -36,49 +32,19 @@ export default class genericController {
     try {
       const companyId = req.user.companyId;
 
-      const jobs = await db['Job'].findAll({
-        where: {
-          companyId: companyId,
-        },
-        attributes: ['status', [db.sequelize.fn('count', db.sequelize.col('status')), 'count']],
-        group: ['Job.status'],
-        raw: true,
-        order: db.sequelize.literal('status DESC')
-      });
-
-      const totalJobs = await db['Job'].count({
+      const jobs = await db['Job'].count({
         where: {
           companyId: companyId,
         },
       });
 
-      const events = await db['Event'].findAll({
-        where: {
-          companyId: companyId,
-        },
-        attributes: ['status', [db.sequelize.fn('count', db.sequelize.col('status')), 'count']],
-        group: ['Event.status'],
-        raw: true,
-        order: db.sequelize.literal('status DESC')
-      });
-
-      const totalEvents = await db['Event'].count({
+      const events = await db['Event'].count({
         where: {
           companyId: companyId,
         },
       });
 
-      const blogs = await db['Blog'].findAll({
-        where: {
-          companyId: companyId,
-        },
-        attributes: ['status', [db.sequelize.fn('count', db.sequelize.col('status')), 'count']],
-        group: ['Blog.status'],
-        raw: true,
-        order: db.sequelize.literal('status DESC')
-      });
-
-      const totalBlogs = await db['Blog'].count({
+      const blogs = await db['Blog'].count({
         where: {
           companyId: companyId,
         },
@@ -86,7 +52,7 @@ export default class genericController {
 
       return res.status(200).json({
         result: {
-          jobs, totalJobs, events, totalEvents, blogs, totalBlogs
+          jobs, events, blogs
         }
       })
     } catch (err) {
