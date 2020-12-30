f808f8825db36e08dca490dd396c0aabdd084812
Aline Abimana <aline@techclick.rw>
2020-12-30 23:27:54 +0200

    Seed of business activities for enablers

---
+++ src/seeders/20201230211844-activitiesForEnablers.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert(
+      "BusinessActivities",
+      [
+        { name: "Co-working spaces", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Business networks", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Accelerators", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Government agencies", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Ecosystem builders", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Financer/Investors", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Academic/Research institutions", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Incubators", createdAt: new Date(), updatedAt: new Date()},
+        { name: "Talent Development", createdAt: new Date(), updatedAt: new Date()},
+      ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("BusinessActivities", null, {});
+  },
+};
