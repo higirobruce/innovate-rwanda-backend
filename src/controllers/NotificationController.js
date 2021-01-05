91e3c5a37004f34936ae5539e795c1d06c2e0c6b
Aline Abimana <aline@techclick.rw>
2021-01-05 23:40:33 +0200

    More fields on notifications list

--- src/controllers/NotificationController.js
+++ src/controllers/NotificationController.js
@@ -47,7 +47,7 @@ export default class NotificationController {
       const user = req.user;
       const notifications = await db['Notification'].findAll({
         where: { companyId: user.companyId, },
-        attributes: ["id", "subject", "content"],
+        attributes: ["id", "subject", "content", "createdAt", "firstread"],
         order: [['createdAt', 'DESC']]
       });
       if (notifications && notifications.length > 0) {
