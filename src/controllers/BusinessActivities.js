c7661336397cac275eafe35ec7cc37b45c0e96f6
Aline Abimana <aline@techclick.rw>
2021-01-06 08:20:36 +0200

    Improvement to business activity deletion to remove it where used

--- src/controllers/BusinessActivities.js
+++ src/controllers/BusinessActivities.js
@@ -33,15 +33,31 @@ export default class BusinessActivities {
 
   static async removeActivity(req, res) {
     try {
+      const activityId = req.query.activityId;
+
+      await db["ActivitiesOfCompany"].destroy({
+        where: { activityId: activityId }
+      });
+
+      await db["AudienceForPost"].destroy({
+        where: { activityId: activityId }
+      });
+
+      await db["Company"].update(
+        { businessActivityId: null }, { where: { businessActivityId: activityId } }
+      );
+
       const response = await db["BusinessActivities"].destroy({
-        where: { id: req.query.activityId }
-      })
-      if (response) {
+        where: { id: activityId }
+      });
+      
+      if (response === 1) {
         return res.status(200).json({ message: "Activity Removed" })
       } else {
         return res.status(200).json({ message: "Activity not yet added" })
       }
-    } catch (err) {
+    } catch (error) {
+      console.log(error)
       return res.status(400).send({ message: "Sorry, Failed to remove business activity at moment" });
     }
   }
