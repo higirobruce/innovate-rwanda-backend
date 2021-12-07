40e8515455b2db218ff10ba01316f4f59816a499
Gilles Kagarama <kagaramag@gmail.com>
2021-12-07 16:54:05 +0200

    throw specific error when token expires

--- src/middlewares/authorization_authentication.js
+++ src/middlewares/authorization_authentication.js
@@ -1,25 +1,24 @@
-import db from "../models";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import db from '../models';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const getToken = (req, res) => {
-
   const token = jwt.sign({ user: res.locals.user }, process.env.SECRETKEY, {
-    expiresIn: "1d",
+    expiresIn: '1d',
   });
   if (token) {
     return res.status(200).send({
-      message: "You are logged in successfully",
+      message: 'You are logged in successfully',
       user: res.locals.user,
       token: token,
       companyInfo: res.locals.companyInfo,
     });
   } else {
     return res.status(403).send({
-      message: "Invalid email, password or company information",
+      message: 'Invalid email, password or company information',
       user: res.locals.user,
-      token: "",
+      token: '',
       companyInfo: res.locals.companyInfo,
     });
   }
@@ -28,15 +27,19 @@ const getToken = (req, res) => {
 const verifyToken = (req, res, next) => {
   var token = req.headers['x-access-token'] || req.headers['authorization'];
   if (!token) {
-    return res.status(401).send({ error: "Unauthorized access" });
+    return res.status(401).send({ error: 'Unauthorized access' });
   }
   try {
-    token = token.replace(/^Bearer\s+/, "");
+    token = token.replace(/^Bearer\s+/, '');
     jwt.verify(token, process.env.SECRETKEY, (err, decoded) => {
       if (err) {
-        return res.status(403).send({
-          message: "Unauthorized access",
-        });
+        return err.name == 'TokenExpiredError'
+          ? res.status(401).send({
+              message: 'TokenExpiredError',
+            })
+          : res.status(403).send({
+              message: 'Unauthorized access',
+            });
       } else {
         req.user = decoded.user;
         next();
@@ -44,7 +47,7 @@ const verifyToken = (req, res, next) => {
     });
   } catch (error) {
     return res.status(401).send({
-      message: "Invalid email, password or company information",
+      message: 'Invalid email, password or company information',
     });
   }
 };
